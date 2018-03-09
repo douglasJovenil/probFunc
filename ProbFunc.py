46091b606d993ab41f76eba116e4b0120a18b21f
douglasJovenil <douglascastilhos@yahoo.com.br>
2018-03-09 14:57:24 -0300

    Ordenado o vetor na mediana

--- ProbFunc.py
+++ ProbFunc.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as pyplot
 from functools import reduce
 from pandas import read_csv
+import numpy
 
 def getData(index):
    return tuple(read_csv("Janeiro.csv", sep=";").iloc[:, index])
@@ -10,6 +12,9 @@ def media(vetor):
 def mediana(vetor, i=0):
    if i == int(len(vetor) / 2): 
       if i % 2 == 0: return vetor[i]
-      else: return media((int(vetor[i]), vetor[i+1]))
+      else: return media((vetor[i]), vetor[i + 1])
+   return mediana(tuple(sorted(vetor)), i + 1)
 
-   return mediana(vetor, i + 1)
+def histograma(vetor):
+   pyplot.hist(numpy.array([vetor]))
+   
